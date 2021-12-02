48b169b0d43d3b876632509975da95ca9292f56d
Mattias Lundberg <me@mlundberg.se>
2021-12-02 19:02:17 +0100

    Refactor solution

--- day01.el
+++ day01.el
@@ -1,25 +1,31 @@
 (require 'dash)
 
-(defun read-lines (filePath)
-  "Return a list of lines of a file at filePath."
+(defun read-lines (file-path)
   (with-temp-buffer
-    (insert-file-contents filePath)
+    (insert-file-contents file-path)
     (split-string (buffer-string) "\n" t)))
 
-(let* ((testinput (read-lines "./input/day01.txt"))
-       (testinput (-map #'string-to-number testinput))
-       ;; (testinput '(199 200 208 210 200 207 240 269 260 263))
-       (pairs (-zip-lists testinput (cons 999999 testinput)))
-       (increases (--filter (> (car it) (nth 1 it)) pairs)))
+(defun read-lines-num (file-path)
+  (let ((lines (read-lines file-path)))
+    (-map #'string-to-number lines)))
+
+(defun get-increases (input)
+  (let* ((second (cons 9999999 input))
+         (pairs (-zip-lists input second)))
+      (--filter (> (car it) (nth 1 it)) pairs)))
+
+(defun sum-sublists (lists)
+  (let ((summed (--map (-reduce #'+ it) lists)))
+    (cdr (cdr summed))))
+
+(let* ((testinput (read-lines-num "./input/day01.txt"))
+       (increases (get-increases testinput)))
   (message "Part 1: %d" (length increases)))
 
-(let* ((testinput (read-lines "./input/day01.txt"))
-       (testinput (-map #'string-to-number testinput))
-       ;; (testinput '(199 200 208 210 200 207 240 269 260 263))
-       (triplets (-zip-lists testinput (cons 999999 testinput) (cons 99999 (cons 999999 testinput))))
-       (sums (--map (-reduce #'+ it) triplets))
-       (sums (cdr (cdr sums)))
-       (pairs (-zip-lists sums (cons 999999 sums)))
-       (increases (--filter (> (car it) (nth 1 it)) pairs)))
-  ;; (message pairs)
+(let* ((testinput (read-lines-num "./input/day01.txt"))
+       (input-one (cons 9999999 testinput))
+       (input-two (cons 9999999 input-one))
+       (triplets (-zip-lists testinput input-one input-two))
+       (sums (sum-sublists triplets))
+       (increases (get-increases sums)))
   (message "Part 2: %d" (length increases)))
