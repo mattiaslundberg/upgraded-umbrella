10a2178de1b5c4f377d03d534635f5a113b8f7ed
Mattias Lundberg <me@mlundberg.se>
2021-12-07 07:54:27 +0100

    Day 7 part 2

--- day07.py
+++ day07.py
@@ -11,3 +11,15 @@ for i in range(1000):
     min_fuel = min(fuel, min_fuel)
 
 print(f"Part 1 {min_fuel}")
+
+min_fuel = 99999999999
+
+for i in range(1000):
+    fuel = 0
+    for position in positions:
+        moves = abs(position - i)
+        fuel += int((moves * (moves + 1)) / 2)
+
+    min_fuel = min(fuel, min_fuel)
+
+print(f"Part 2 {min_fuel}")
