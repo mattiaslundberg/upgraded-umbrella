310690fc5db9c498a2042c724965fc3a400c4dd5
Mattias Lundberg <me@mlundberg.se>
2021-12-09 08:41:13 +0100

    Day 9 part 2

--- day09.py
+++ day09.py
@@ -8,7 +8,7 @@ with open("./input/day09.txt") as f:
 
 
 def low_points(depths):
-    res = []
+    res = {}
     for x in range(len(depths)):
         for y in range(len(depths[0])):
             curr = depths[x][y]
@@ -21,10 +21,45 @@ def low_points(depths):
             elif y > 0 and curr >= depths[x][y - 1]:
                 pass
             else:
-                res.append(curr)
+                res[(x, y)] = curr
     return res
 
 
-low = low_points(lines)
+lows = low_points(lines)
 
-print(f"Part 1: {len(low) + sum(low)}")
+print(f"Part 1: {len(lows.values()) + sum(lows.values())}")
+
+def find_basin(x, y, excluded):
+    if (x, y) in excluded:
+        return None, 0
+    if x < 0 or y < 0:
+        return None, 0
+    if x == len(lines) or y == len(lines[0]):
+        return None, 0
+    if lines[x][y] == 9:
+        return None, 0
+
+    size = 1
+    excluded += [(x, y)]
+    for xc, yc in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
+        e, s = find_basin(xc, yc, excluded)
+        size += s
+        if s > 0 and (xc, yc) not in excluded:
+            excluded += e
+
+    return excluded, size
+
+
+
+def find_basins(lows):
+    res = []
+    for xs, ys in lows.keys():
+        r, basin = find_basin(xs, ys, [])
+        res.append(basin)
+    res = sorted(res, reverse=True)
+    return res[:3]
+
+
+[a, b, c] = find_basins(lows)
+
+print(f"Part 2: {a * b * c}")
