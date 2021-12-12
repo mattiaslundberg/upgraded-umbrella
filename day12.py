b7445b4b23f2a29154ebab987717f744c89d0ef2
Mattias Lundberg <me@mlundberg.se>
2021-12-12 09:14:24 +0100

    Day 12 part 2

--- day12.py
+++ day12.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 with open("./input/day12.txt") as f:
     raw = f.readlines()
@@ -13,21 +13,31 @@ for a, b in edges:
     paths[b].append(a)
 
 
-def find_path(location, visited_small):
+def find_path(location, visited_small, max_small_visits):
     if location == "end":
         return 1
-    if location in visited_small:
+    if visited_small["start"] >= 1 and location == "start":
+        return 0
+    if visited_small[location] >= max_small_visits:
         return 0
 
-    nexts = paths[location]
     if location.islower():
-        visited_small = visited_small + [location]
+        visited_small = visited_small.copy()
+        visited_small[location] += 1
+
+        c = Counter(visited_small.values())
+        if visited_small[location] >= max_small_visits and c[2] >= 2:
+            return 0
 
     result = 0
+    nexts = paths[location]
     for n in nexts:
-        result += find_path(n, visited_small)
+        result += find_path(n, visited_small, max_small_visits)
     return result
 
 
-result = find_path("start", [])
+result = find_path("start", defaultdict(int), 1)
 print(f"Part 1: {result}")
+
+result = find_path("start", defaultdict(int), 2)
+print(f"Part 2: {result}")
