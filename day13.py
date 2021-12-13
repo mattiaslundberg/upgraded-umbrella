8bc97f053476dc11197497b64ad34a489cfc9166
Mattias Lundberg <me@mlundberg.se>
2021-12-13 07:47:50 +0100

    Day 13 part 2

--- day13.py
+++ day13.py
@@ -1,3 +1,5 @@
+import sys
+
 with open("./input/day13.txt") as f:
     raw = f.readlines()
 
@@ -35,3 +37,23 @@ def do_fold(direction, location, dots):
 next_dots = do_fold(direction, location, dots)
 result = sum(next_dots.values())
 print(f"Part 1: {result}")
+
+final_dots = dots.copy()
+for (direction, location) in folds:
+    final_dots = do_fold(direction, location, final_dots)
+
+max_x = 0
+max_y = 0
+for (x, y), v in final_dots.items():
+    max_x = max(x, max_x)
+    max_y = max(y, max_y)
+
+print("#### Part 2 ####")
+for y in range(max_y + 1):
+    for x in range(max_x + 1):
+        if (x, y) in final_dots:
+            sys.stdout.write("#")
+        else:
+            sys.stdout.write(" ")
+    print()
+print("####        ####")
