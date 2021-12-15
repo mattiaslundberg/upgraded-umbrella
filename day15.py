ab3104e8cfbe472930e6b84a9e9148cf89fb0ed5
Mattias Lundberg <me@mlundberg.se>
2021-12-15 12:52:30 +0100

    Day 15 part 2

--- day15.py
+++ day15.py
@@ -1,8 +1,9 @@
 import sys
+from copy import deepcopy
 from functools import lru_cache
+from queue import PriorityQueue
 
 sys.setrecursionlimit(5000)
-
 
 with open("./input/day15.txt") as f:
     raw = f.readlines()
@@ -11,90 +12,64 @@ with open("./input/day15.txt") as f:
         risks.append(list(map(int, list(r.strip()))))
 
 
-@lru_cache
-def find_path(location, goal):
-    (x, y) = location
-    (gx, gy) = goal
-    risk = risks[x][y]
-
-    if x == gx and y == gy:
-        return risk
-
-    min_path = 9999999
-    if x < len(risks) - 1:
-        min_path = min(min_path, find_path((x + 1, y), goal))
-    if y < len(risks[0]) - 1:
-        min_path = min(min_path, find_path((x, y + 1), goal))
-    if x == 0 and y == 0:
-        return min_path
-    return min_path + risk
-
-
-location = (0, 0)
-goal = (len(risks) - 1, len(risks[0]) - 1)
-
-
-print(f"Part 1: {find_path(location, goal)}")
-
-
 def inc(n):
-    if n >= 9:
-        return 1
-    else:
-        return n + 1
-
-
-def increase(locs):
-    r = []
-    for l in locs:
-        ri = []
-        for c in l:
-            ri.append(inc(c))
-        r.append(ri)
-    return r
+    if n >= 9: return 1
+    else: return n + 1
 
 
 def increase_row(locs):
     return [inc(n) for n in locs]
 
 
-risks2 = risks.copy()
-previous = risks2
+tmp = deepcopy(risks)
+previous = tmp
 for i in range(4):
-    nn = increase(previous)
+    nn = [increase_row(l) for l in previous]
     previous = nn
-    risks2 += nn
+    tmp += nn
 
-risks3 = []
-for l in risks2:
+risks2 = []
+for l in tmp:
     res = l
     previous = l
     for _ in range(4):
         nn = increase_row(previous)
         previous = nn
         res += nn
-    risks3.append(res)
-risks2 = risks3
+    risks2.append(res)
 
 
-@lru_cache
-def find_path2(location, goal):
-    (x, y) = location
+
+def find_path(location, goal, r):
     (gx, gy) = goal
-    risk = risks2[x][y]
+    distances = {}
 
-    if x == gx and y == gy:
-        return risk
-
-    min_path = 9999999999999
-    if x < len(risks2) - 1:
-        min_path = min(min_path, find_path2((x + 1, y), goal))
-    if y < len(risks2[0]) - 1:
-        min_path = min(min_path, find_path2((x, y + 1), goal))
-    if x == 0 and y == 0:
-        return min_path
-    return min_path + risk
+    to_visit = PriorityQueue()
+    to_visit.put((0, location))
 
 
+    for x in range(len(r)):
+        for y in range(len(r[0])):
+            distances[(x, y)] = 9999999999
+
+    distances[location] = 0
+
+    while not to_visit.empty():
+        (dist, current) = to_visit.get()
+        (x, y) = current
+
+        for (nx, ny) in [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]:
+            if nx < 0 or ny < 0 or nx > gx or ny > gy:
+                continue
+            alt = distances[current] + r[nx][ny]
+            if alt < distances[(nx, ny)]:
+                distances[(nx, ny)] = alt
+                to_visit.put((alt, (nx, ny)))
+
+    return distances[goal]
+
+location = (0, 0)
+goal = (len(risks) - 1, len(risks[0]) - 1)
+print(f"Part 1: {find_path(location, goal, risks)}")
 goal = (len(risks2) - 1, len(risks2[0]) - 1)
-print(f"Part 2: {find_path2(location, goal)}")
+print(f"Part 2: {find_path(location, goal, risks2)}")
